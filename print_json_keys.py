55ab2b060ae9c092df4124ecf8e66c06be3b183e
Stepan Yakimovich <xyakimo1@fi.muni.cz>
2022-06-12 21:37:31 +0200

    Convinient read of parse.py output file

---
+++ print_json_keys.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import sys
+import json
+
+YELLOW='\033[1;33m'
+PURPLE='\033[0;35m'
+NO_COLOR='\033[0m'
+
+if len(sys.argv) != 2:
+    print("USAGE: ./print_json_keys.py <keys>.\nWhere <keys> is path to a JSON file generated with parse.py", file = sys.stderr)
+    exit(1)
+
+file = open(sys.argv[1])
+dict = json.load(file)
+file.close()
+
+def draw_pipeline(spaces: int): # it's hard to give reasonable names, but I hope it's not that important.
+    print(' ' * spaces, end = '')
+    print("|--> ", end = '')
+
+key_count = 0
+for key in dict.keys():
+    key_count += 1
+    print(key_count, ". Key: ", YELLOW, key, NO_COLOR, sep = '')
+
+    tx_count = 0
+    for tx in dict[key]:
+        tx_count += 1
+        draw_pipeline(key_count // 10 + len(". K"))
+        print(tx_count, ". Transaction: ", PURPLE, tx["ID"], NO_COLOR, sep = '')
+        """ # Uncomment to print time and signature
+        draw_pipeline(key_count // 10 + len(". K") + tx_count//10 + len(". T") + len("|--> "))
+        print("Time: ", tx["time"], sep = '')
+        draw_pipeline(key_count // 10 + len(". K") + tx_count//10 + len(". T") + len("|--> "))
+        print("Sigature: ", tx["signature"], sep = '')
+        """
+
+exit(0)
