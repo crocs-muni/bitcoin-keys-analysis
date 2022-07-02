e82017c1b883650a01f92e87a77729f0cadf81bf
Stepan Yakimovich <xyakimo1@fi.muni.cz>
2022-07-02 17:38:12 +0300

    Fixed output to a wrong file, found a issue with signatures (see notes.txt)

--- parse.py
+++ parse.py
@@ -34,7 +34,6 @@ class Parser:
                             Parser.keys += 1
                             Parser.saved_data[suspected_key] = []
                         Parser.saved_data[suspected_key].append({'ID' : transaction['txid'], 'time' : transaction['time'], 'signature' : signature})
-                        #print(Parser.saved_data[suspected_key])
                         toreturn = True
         return toreturn
 
@@ -69,7 +68,7 @@ class Parser:
         return toreturn
     
     # This function tries to process a Pay to Script hash transaction. This is a newer type of transaction with seceral subtypes
-    # ScriptSig: Contains signatures. Last entry is the unlocking script that needs to be further parsed to extract publick key
+    # ScriptSig: Contains signatures. Last entry is the unlocking script that needs to be further parsed to extract public key
     # Locking script: contains a few instructions and hash of script that unlocks the spending
     # returns true if key was extracted
     def process_transaction_p2sh(self, transaction):
@@ -97,16 +96,18 @@ class Parser:
                     if (redeem_script[-1] == 'e') and (redeem_script[-2] == 'a'): # Found checkmultisig instruction so multiple keys key will be in front of it"
                         if (redeem_script[0] == '1' and redeem_script[1] == '4'): # Transactions found in block 570006 that break the script completely
                             return False
-                        num_sigs = int(redeem_script[1]) # Checking the number of signatures present
+
+                        num_sigs = int(redeem_script[1], 16) # Checking the number of signatures present
                         redeem_script = redeem_script[2:-2] # Cutting instructions at beginning and end"
-                        num_keys = int(redeem_script[-1]) # Bit hacky but should work, format should be num of signatures required pubkey1 ... pubkeyn num of all pubkeys
+                        num_keys = int(redeem_script[-1], 16) # Bit hacky but should work, format should be num of signatures required pubkey1 ... pubkeyn num of all pubkeys
                         redeem_script = redeem_script[:-2] # Cutting counter at beginning and end"
+
                         if (num_sigs == num_keys):
                             for i in range(num_keys):
                                 signature = vin['scriptSig']['asm'].replace("[ALL]", "").split(" ")[i+1] # Skipping over the first 0 here as well 
                                 key_len = int(redeem_script[:2], 16)
-                                suspected_key = redeem_script[2:(key_len + 1)*2]
-                                redeem_script = redeem_script[(2 + (key_len*2)):]
+                                suspected_key = redeem_script[2:(key_len*2) + 2]
+                                redeem_script = redeem_script[((key_len*2) + 2):]
                                 if (len(suspected_key) in (66, 130)) and (suspected_key[0] == '0') and (suspected_key[1] in ('2', '3', '4')):
                                     if suspected_key not in Parser.saved_data.keys():
                                         if (len(suspected_key) == 66):
@@ -121,23 +122,25 @@ class Parser:
                             sigs = []
                             for j in range(num_sigs):
                                 signature = vin['scriptSig']['asm'].replace("[ALL]","").split(" ")[j+1]
+                                #print(len(signature))  See notes.txt
+                                #print(signature)
                                 if len(signature) not in (148, 144, 146, 142):
                                     signature = "NaN"
                                 sigs.append(signature)
                             for i in range(num_keys):
                                 key_len = int(redeem_script[:2], 16)
-                                suspected_key = redeem_script[2:(key_len + 1)*2]
-                                redeem_script = redeem_script[(2 + (key_len*2)):]
+                                suspected_key = redeem_script[2:(key_len*2) + 2]
+                                redeem_script = redeem_script[((key_len*2) + 2):]
                                 if (len(suspected_key) in (66, 130)) and (suspected_key[0] == '0') and (suspected_key[1] in ('2', '3', '4')):
-                                    if suspected_key not in Parser.unmatched_data.keys():
+                                    if suspected_key not in Parser.saved_data.keys():
                                         if (len(suspected_key) == 66):
                                             Parser.short += 1
                                         Parser.keys += 1
-                                        Parser.unmatched_data[suspected_key] = []
-                                    Parser.unmatched_data[suspected_key].append({'ID' : transaction['txid'], 'time' : transaction['time'], 'signatures' : sigs})
-                                    toreturn = True
-                            
-                        
+                                        Parser.saved_data[suspected_key] = []
+                                    Parser.saved_data[suspected_key].append({'ID' : transaction['txid'], 'time' : transaction['time'], 'signatures' : sigs})
+                                    toreturn = True                            
+
+
         return toreturn
     
     # This function tries to process a Pay to Witness Public Key transaction. Those are new SegWit transactions
@@ -251,7 +254,6 @@ class Parser:
             block_hash = rpc.getblockhash(n)
             block_transactions = rpc.getblock(block_hash)['tx']
             for transaction_hash in block_transactions: # iterating over all transactions in a block
-                #print("Transaction hash:", transaction_hash)
                 Parser.inputs += 1
                 transaction = rpc.getrawtransaction(transaction_hash, True) # Getting transaction in verbose format to get all the needed parsed details
                 try:
@@ -278,4 +280,4 @@ class Parser:
 
 #Example of use:
 parser = Parser()
-parser.process_blocks(277069, 277070)
+parser.process_blocks(405987, 405988)
