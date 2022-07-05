17728b3e11fcc8bc6a603d313f24e81c08ef0db8
Stepan Yakimovich <xyakimo1@fi.muni.cz>
2022-07-05 12:14:16 +0200

    Delete debug.sh

--- debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#   Run as ". debug.sh" not to export 'cli' function and other variables
-
-cli "getrawtransaction" "00e07f279dd05b9b68c40f21b43c57847e75c35cd3bbc2d80921eb037ef0c9a8" "true"
