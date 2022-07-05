8e52243cf28821318c0c5e4b170810d4b5b35238
Stepan Yakimovich <xyakimo1@fi.muni.cz>
2022-07-05 13:11:51 +0300

    Changed return to exit

--- run_daemon.sh
+++ run_daemon.sh
@@ -7,7 +7,7 @@ CLIENT_DIR="/home/xyakimo1/crocs/bitcoin-client/bin"
 DATA_DIR="/home/xyakimo1/crocs/.bitcoin-data"
 CONF=$DATA_DIR/bitcoin.conf
 
-$CLIENT_DIR/bitcoind -datadir="$DATA_DIR" -conf="$CONF" || ( echo -ne "\007 \007 \007" && return 1 )
+$CLIENT_DIR/bitcoind -datadir="$DATA_DIR" -conf="$CONF" || ( echo -ne "\007 \007 \007" && exit 1 )
 
 cli()
 {
