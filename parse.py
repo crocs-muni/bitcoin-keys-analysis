77b109002793ce5ab9bededb9c366896f06c5c5b
Stepan Yakimovich <xyakimo1@fi.muni.cz>
2022-07-21 14:02:55 +0300

    Full decomposition of P2WSH

--- parse.py
+++ parse.py
@@ -37,11 +37,11 @@ class Parser:
         signature = vin['scriptSig']['asm'].split(" ")[0]
         signature = signature.replace("[ALL]", "")
         # A function like remove_sighash_flags() is needed, which will remove all flags, not only [ALL].
-        # But idk what are they in Bitcoin Core asm format.  
+        # But idk what are they in Bitcoin Core asm format. 
         # Or just cut off last byte?
 
         if len(signature) not in Parser.SIGNATURE_LENGTHS:
-            print("Failed signature:", signature)
+            #print("Failed signature:", signature)
             signature = "NaN"
 
         return signature
@@ -108,24 +108,45 @@ class Parser:
 
         return toreturn
 
+    # I suppose these two functions can be connected to one?
+    def extract_signature_p2sh_checksig(self, vin):
+        signature = vin['scriptSig']['asm'].split("[ALL] ")[0].split(" ")[1] # Skipping an extra zero here that was added due to bugs
+        if len(signature) not in Parser.SIGNATURE_LENGTHS:
+            signature = "NaN"
+        return signature
 
-    # Make me more clean pls
-    def handle_checksig(self, transaction, vin, script):
+    def extract_signature_p2sh_multisig(self, vin, i):
+        signature = vin['scriptSig']['asm'].replace("[ALL]", "").split(" ")[i+1] # Skipping over the first 0 here as well
+        if len(signature) not in Parser.SIGNATURE_LENGTHS:
+            signature = "NaN"
+        return signature
+
+    # In case of checksig pass i = 0
+    def extract_signature_p2wsh(self, vin, i):
+        signature = vin['txinwitness'][i + 1] # Skipping the empty item
+        if len(signature) not in Parser.SIGNATURE_LENGTHS:
+            signature = "NaN"
+        return signature
+
+
+
+    def handle_checksig(self, transaction, vin, script, transaction_type):
         if(' ' in script):
             suspected_key = script.split(' ')[0] # blocks in 2019 get parsed with signature[all] pubkey somescript
         else:
             suspected_key = script[:-2]
 
-        signature = vin['scriptSig']['asm'].split("[ALL] ")[0].split(" ")[1] # Skipping an extra zero here that was added to BTC script due to bugs
-        if len(signature) not in Parser.SIGNATURE_LENGTHS:
-            signature = "NaN"
+        if transaction_type == "P2SH":
+            signature = Parser.extract_signature_p2sh_checksig(self, vin)
+        if transaction_type == "P2WSH":
+            signature = Parser.extract_signature_p2wsh(self, vin, 0)
 
         if (Parser.correct_ecdsa_key(self, suspected_key)):
             Parser.add_key_to_saved_data(self, transaction, suspected_key, signature)
             return True
 
-
-    def handle_checkmultisig(self, transaction, vin, script):
+    # Make me more clean pls
+    def handle_checkmultisig(self, transaction, vin, script, transaction_type):
         toreturn = False
         if (script[0] == '1' and script[1] == '4'): # Transactions found in block 570006 that break the script completely
             return False
@@ -138,9 +159,10 @@ class Parser:
         if (num_sigs == num_keys):
             for i in range(num_keys):
 
-                signature = vin['scriptSig']['asm'].replace("[ALL]", "").split(" ")[i+1] # Skipping over the first 0 here as well 
-                if len(signature) not in Parser.SIGNATURE_LENGTHS:
-                    signature = "NaN"
+                if transaction_type == "P2SH":
+                    signature = Parser.extract_signature_p2sh_multisig(self, vin, i)
+                if transaction_type == "P2WSH":
+                    signature = Parser.extract_signature_p2wsh(self, vin, i)
 
                 key_len = int(script[:2], 16)
                 suspected_key = script[2:(key_len*2) + 2]
@@ -153,11 +175,12 @@ class Parser:
         else:
             sigs = []
             for j in range(num_sigs):
-                signature = vin['scriptSig']['asm'].replace("[ALL]","").split(" ")[j+1]
-                #print(len(signature))  See notes.txt
-                #print(signature)
-                if len(signature) not in Parser.SIGNATURE_LENGTHS:
-                    signature = "NaN"
+
+                if transaction_type == "P2SH":
+                    signature = Parser.extract_signature_p2sh_multisig(self, vin, j)
+                if transaction_type == "P2WSH":
+                    signature = Parser.extract_signature_p2wsh(self, vin, j)
+
                 sigs.append(signature)
 
             for i in range(num_keys):
@@ -173,14 +196,18 @@ class Parser:
 
     # This function <<saves>> found data (if any) to Parser.saved_data or Parser.unmatched_data.
     # Returns true at least one key was extracted.
-    def parse_serialized_script(self, transaction, vin):
-        script = vin['scriptSig']['asm'].split(" ")[-1]
+    # Note that <transaction_type> argument is one of these: "P2SH", "P2WSH".
+    def parse_serialized_script(self, transaction, vin, script, transaction_type):
+
+        if transaction_type != "P2SH" and transaction_type != "P2WSH":
+            print("You have mistake in your code. Please read caption of parse_serialized_script().")
+            return False
 
         if script[-1] == 'c' and script[-2] == 'a': # Checksig instruction in hex is "ac"
-            return Parser.handle_checksig(self, transaction, vin, script)
+            return Parser.handle_checksig(self, transaction, vin, script, transaction_type)
 
         if script[-1] == 'e' and script[-2] == 'a': # Checkmultisig instruction in hex is "ae"
-            return Parser.handle_checkmultisig(self, transaction, vin, script)
+            return Parser.handle_checkmultisig(self, transaction, vin, script, transaction_type)
 
         return False
 
@@ -198,7 +225,8 @@ class Parser:
             if len(vin['scriptSig']['asm'].split(" ")) < 2: #splitting on the separator, len should be 2 or more"
                 continue
 
-            if Parser.parse_serialized_script(self, transaction, vin):
+            script = vin['scriptSig']['asm'].split(" ")[-1]
+            if Parser.parse_serialized_script(self, transaction, vin, script, "P2SH"):
                 toreturn = True
                 
         return toreturn
@@ -235,62 +263,17 @@ class Parser:
     def process_transaction_p2wsh(self, transaction):
         toreturn = False
         for vin in transaction['vin']:
-            if 'txinwitness' in vin.keys():
-                if (len(vin['txinwitness']) > 1):
-                    redeem_script = vin['txinwitness'][-1]
-                    if (redeem_script[-1] == 'c') and (redeem_script[-2] == 'a'): # Found checksig instruction so one key will be in front of it"
-                        signature = vin['txinwitness'][1] # Skipping the empty item
-                        suspected_key = redeem_script[:-2]
-                        if (len(suspected_key) in (66, 130)) and (suspected_key[0] == '0') and (suspected_key[1] in ('2', '3', '4')):
-                            if suspected_key not in Parser.saved_data.keys():
-                                if (suspected_key[1] in ('2', '3')):
-                                    Parser.short += 1
-                                Parser.keys += 1
-                                Parser.saved_data[suspected_key] = []
-                            if len(signature) not in Parser.SIGNATURE_LENGTHS:
-                                signature = "NaN"
-                            Parser.saved_data[suspected_key].append({'ID' : transaction['txid'], 'time' : transaction['time'], 'signature' : signature})
-                            toreturn = True
-                    if (redeem_script[-1] == 'e') and (redeem_script[-2] == 'a'): # Found checkmultisig instruction so multiple keys key will be in front of it"
-                        num_sigs = int(redeem_script[1], 16) # Checking the number of signatures present
-                        redeem_script = redeem_script[2:-2] # Cutting instructions at beginning and end"
-                        num_keys = int(redeem_script[-1], 16) # Bit hacky but should work, format should be num of verifications required pubkey1 ... pubkeyn num of all pubkeys
-                        redeem_script = redeem_script[:-2] # Cutting counter at beginning and end"
-                        if (num_sigs == num_keys):
-                            for i in range(num_keys):
-                                signature = vin['txinwitness'][i + 1]
-                                key_len = int(redeem_script[:2], 16)
-                                suspected_key = redeem_script[2:(key_len + 1)*2]
-                                redeem_script = redeem_script[(2 + (key_len*2)):]
-                                if (len(suspected_key) in (66, 130)) and (suspected_key[0] == '0') and (suspected_key[1] in ('2', '3', '4')):
-                                    if suspected_key not in Parser.saved_data.keys():
-                                        if (len(suspected_key) == 66):
-                                            Parser.short += 1
-                                        Parser.keys += 1
-                                        Parser.saved_data[suspected_key] = []
-                                    if len(signature) not in Parser.SIGNATURE_LENGTHS:
-                                        signature = "NaN"
-                                    Parser.saved_data[suspected_key].append({'ID' : transaction['txid'], 'time' : transaction['time'], 'signature' : signature})
-                                    toreturn = True
-                        else:
-                            sigs = []
-                            for j in range(num_sigs):
-                                signature = vin['txinwitness'][j + 1]
-                                if len(signature) not in Parser.SIGNATURE_LENGTHS:
-                                    signature = "NaN"
-                                sigs.append(signature)
-                            for i in range(num_keys):
-                                key_len = int(redeem_script[:2], 16)
-                                suspected_key = redeem_script[2:(key_len + 1)*2]
-                                redeem_script = redeem_script[(2 + (key_len*2)):]
-                                if (len(suspected_key) in (66, 130)) and (suspected_key[0] == '0') and (suspected_key[1] in ('2', '3', '4')):
-                                    if suspected_key not in Parser.unmatched_data.keys():
-                                        if (len(suspected_key) == 66):
-                                            Parser.short += 1
-                                        Parser.keys += 1
-                                        Parser.unmatched_data[suspected_key] = []
-                                    Parser.unmatched_data[suspected_key].append({'ID' : transaction['txid'], 'time' : transaction['time'], 'signatures' : sigs})
-                                    toreturn = True
+
+            if not 'txinwitness' in vin.keys():
+                continue
+
+            if (len(vin['txinwitness']) < 2):
+                continue
+
+            script = vin['txinwitness'][-1]
+            if Parser.parse_serialized_script(self, transaction, vin, script, "P2WSH"):
+                toreturn = True
+
         return toreturn
  
 
