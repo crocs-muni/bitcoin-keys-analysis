283fe6d1475f760bbf66dc8213d3bc3b2b31b016
Stepan Yakimovich <xyakimo1@fi.muni.cz>
2022-08-10 19:21:01 +0300

    Tailored prepare_keys.py to my data structure and Schnorr keys.

--- prepare_keys.py
+++ prepare_keys.py
@@ -1,15 +1,37 @@
 #!/bin/python3
-
 import json, os
-k = set()
-for filename in os.listdir(os.curdir):
-	if "data" in filename:
-		f = open(filename, "r")
-		data = json.load(f).keys()
-		for textkey in data:
-			item = bytes.fromhex(textkey[2:34])
-			k.add(item)
-keys_file = open("keys_set", "wb")
-for key in k:
-	keys_file.write(key)
+
+ecdsa_key_set = set()
+schnorr_key_set = set()
+DIR = "/home/xyakimo1/crocs/gathered-data/"
+
+for filename in os.listdir(DIR):
+
+    filename = DIR + filename
+    f = open(filename, "r")
+    data = json.load(f).keys()
+
+    if "ecdsa" in filename:
+        for textkey in data:
+            item = bytes.fromhex(textkey[2:34])
+            ecdsa_key_set.add(item)
+
+    if "schnorr" in filename:
+        for textkey in data:
+            item = bytes.fromhex(textkey[2:34])
+            schnorr_key_set.add(item)
+
+    f.close()
+
+
+keys_file = open("ecdsa_keys_set", "wb")
+for key in ecdsa_key_set:
+    keys_file.write(key)
 keys_file.close()
+print(len(ecdsa_key_set), "ECDSA keys have been written to <ecdsa_keys_set>")
+
+keys_file = open("schnorr_keys_set", "wb")
+for key in schnorr_key_set:
+    keys_file.write(key)
+keys_file.close()
+print(len(schnorr_key_set), "Schnorr Signature keys have been written to <schnorr_keys_set>")
